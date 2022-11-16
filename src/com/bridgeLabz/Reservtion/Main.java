5c1552c4b0e2692e5b3b7d7cd2e63ec35db9ccf8
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 <tusharpatil22700@gmail.com>
2022-11-15 22:20:57 -0800

    welcome

--- src/com/bridgeLabz/Reservtion/Main.java
+++ src/com/bridgeLabz/Reservtion/Main.java
@@ -1,10 +1,35 @@
 package com.bridgeLabz.Reservtion;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		System.out.println("...welcome to Hotel Reservtion System...");
+		
+		Hotel check =new Hotel();
+        Hotel hotel_1 =new Hotel("LakeWood","Regular", 30,220);
+        Hotel hotel_2 =new Hotel("RidgeWood","Reward", 15, 100);
+        Hotel hotel_3 =new Hotel("Karma","Regular", 20 ,800);
+        Hotel hotel_4 =new Hotel("HanShRaj","Reward", 30,  120);
+        Hotel hotel_5 =new Hotel("DBCity","Reward", 25 ,1500);
+        Hotel hotel_6 =new Hotel("Golden","Regular", 20 ,220);
+        Hotel hotel_7 =new Hotel("Milan","Reward", 10, 80);
+        Hotel hotel_9 =new Hotel("Vmart","Regular", 15, 6000);
+        Hotel hotel_10 =new Hotel("GoldenCity","Regular", 25,150);
+        ArrayList<Hotel>hotels =new ArrayList<>();
+        hotels.add(hotel_1);
+        hotels.add(hotel_2);
+        hotels.add(hotel_3);
+        hotels.add(hotel_4);
+        hotels.add(hotel_5);
+        hotels.add(hotel_6);
+        hotels.add(hotel_7);
+        hotels.add(hotel_9);
+        hotels.add(hotel_10);
+        System.out.println("...................display hotels...............");
+        System.out.println(hotels);
 
 	}
 
