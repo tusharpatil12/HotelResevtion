76fdb9c7ad48958abe33f25a1923a35435acd077
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 <tusharpatil22700@gmail.com>
2022-11-15 21:40:03 -0800

    welcome

---
+++ src/com/bridgeLabz/Reservtion/Main.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz.Reservtion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("...welcome to Hotel Reservtion System...");
+
+	}
+
+}
